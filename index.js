21c1e47128ade27305f30dd89eba24ba2fe89439
Meya Gorbea <meyagorbea@yahoo.com>
2017-12-05 23:06:51 -0800

    add new pages

--- index.js
+++
@@ -1,9 +0,0 @@
-
-var mainText = document.getElementById("mainText")
-var submitBtn = document.getElementById("submitBtn");
-
-function submitClick(){
-    var firebaseRef = firebase.database().ref();
-    var message = mainText.value;
-    firebaseRef.push().set(message);
-}
--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 var mainText = document.getElementById("mainText")
 var submitBtn = document.getElementById("submitBtn");
 
+
 function submitClick(){
     var firebaseRef = firebase.database().ref();
     var message = mainText.value;
